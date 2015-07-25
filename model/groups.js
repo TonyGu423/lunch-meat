8a5e5b946603ba8ea75c9074c88b1ba7f3b5b47a
Daniel Boros <dan@kud0h.com>
2015-07-24 22:50:36 -0700

    Changed the modal to be more bootstrappy

--- model/groups.js
+++ model/groups.js
@@ -1,1 +1,9 @@
 Groups = new Mongo.Collection("groups");
+
+Meteor.methods({
+  joinGroup: function(groupName, driverName, peepName) {
+    db.groups.update(
+      { to: groupName, 'cars.driver': driverName },
+      { $push: { 'cars.$.peeps' : { name: peepName }}})
+  }
+});
