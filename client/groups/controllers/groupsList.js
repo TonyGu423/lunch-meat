b9057a7282d3dcb27fdc7c533844a3b9eebde467
Daniel Boros <dan@kud0h.com>
2015-07-24 21:13:49 -0700

    Simple groups view for testing

--- client/groups/controllers/groupsList.js
+++ client/groups/controllers/groupsList.js
@@ -2,11 +2,11 @@ angular.module('lunchMeat').controller('GroupsCtrl', ['$scope', '$meteor', funct
   $scope.groups = $meteor.collection(Groups);
 
   $scope.createGroup = function(group) {
-    $scope.links.save(group);
+    $scope.groups.save(group);
   }
 
-  $scope.removeLink = function(group) {
+  $scope.removeGroup = function(group) {
     if (confirm("Are you sure?"))
-      $scope.links.remove(group);
+      $scope.groups.remove(group);
   }
 }]);
