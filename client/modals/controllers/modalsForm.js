482db169f154897e0edab217618c0cd65685dfa1
Daniel Boros <dan@kud0h.com>
2015-07-25 14:30:03 -0700

    Bootstrapped the groups list and added ability to join as unassigned

--- client/modals/controllers/modalsForm.js
+++ client/modals/controllers/modalsForm.js
@@ -1,27 +1,48 @@
 angular.module('lunchMeat').controller('ModalsCtrl', ['$scope', '$meteor', function($scope, $meteor) {
   $scope.groups = $meteor.collection(Groups);
+
+  // Used to populate options in 'Join a Group' modal.
   $scope.cars = []
 
-  $scope.getDrivers = function() {
+  $scope.createGroup = function(group) {
+    var newGroup = {
+      'to': group.to,
+      'cars': [group.cars]}
+
+    $scope.groups.save(newGroup);
+  }
+
+  $scope.getCars = function() {
     $meteor.call('getCars', $scope.to).then(
       function (data) {
-        console.log('Success [getDrivers]!', data);
+        console.log('Success! [getCars]', data);
         $scope.cars = data;
       },
       function (error) {
-        console.log('Failed [getDrivers]!', error);
+        console.log('Failed! [getCars]', error);
       }
     );
   };
 
   $scope.joinGroup = function() {
-    $meteor.call('joinGroup', $scope.to, $scope.driver, $scope.name).then(
-      function (data) {
-        console.log('Success [joinGroup]!', data);
-      },
-      function (error) {
-        console.log('Failed [joinGroup]!', error);
-      }
-    );
+    if ($scope.driver == 'Unassigned') {
+      $meteor.call('joinGroupAsUnassigned', $scope.to, $scope.name).then(
+        function (data) {
+          console.log('Success! [joinGroupAsUnassigned]', data);
+        },
+        function (error) {
+          console.log('Failed! [joinGroupAsUnassigned]', error);
+        }
+      );
+    } else {
+      $meteor.call('joinGroup', $scope.to, $scope.driver, $scope.name).then(
+        function (data) {
+          console.log('Success! [joinGroup]', data);
+        },
+        function (error) {
+          console.log('Failed! [joinGroup]', error);
+        }
+      );
+    }
   };
 }]);
--- model/groups.js
+++ model/groups.js
@@ -4,10 +4,14 @@ Meteor.methods({
   joinGroup: function(groupName, driverName, peepName) {
     Groups.update(
       { to: groupName, 'cars.driver': driverName },
-      { $push: { 'cars.$.peeps' : { name: peepName }}})
+      { $push: { 'cars.$.peeps' : { name: peepName }}});
+  },
+  joinGroupAsUnassigned: function(groupName, peepName) {
+    Groups.update(
+      { to: groupName },
+      { $push: { unassigned : { name: peepName }}});
   },
   getCars: function(groupName) {
-    console.log(groupName);
-    return Groups.findOne({ to: groupName }).cars
+    return Groups.findOne({ to: groupName }).cars;
   }
 });
